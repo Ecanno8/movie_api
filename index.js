0416f7b977d05bd00359e569a1a23bf58a9c8ef6
Ecanno8 <ethancannon92@gmail.com>
2023-12-20 13:15:03 -0800

    stack error for methodor //

--- index.js
+++ index.js
@@ -15,19 +15,19 @@ app.get('/secreturl', (req, res) => {
 
 app.use(express.static('public'));
 
-const bodyParser = require('body-parser'),
-    methodOverride = require('method-override');
+//const bodyParser = require('body-parser'),
+// methodOverride = require('method-override');
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+//app.use(bodyParser.urlencoded({
+//extended: true
+//}));
 
-app.use(bodyParser.json());
-app.use(methodOverride());
+//app.use(bodyParser.json());
+//app.use(methodOverride());
 
-app.use((err, req, res, next) => {
-    // logic
-});
+//app.use((err, req, res, next) => {
+// logic
+//});
 
 
 app.listen(8080, () => {

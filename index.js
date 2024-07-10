dcc28c7458db48305ef389c40a5dcb63d1f001e4
Ecanno8 <ethancannon92@gmail.com>
2024-07-10 00:52:24 -0700

    added local host for angular

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'http://localhost:56418', 'https://qmovieflix.netlify.app'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'http://localhost:56418', 'https://qmovieflix.netlify.app', 'http://localhost:4200'];
 
 app.use(cors({
     origin: (origin, callback) => {

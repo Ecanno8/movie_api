e43e81494b9ef5d7cad88a3860be2761ec337044
Ecanno8 <ethancannon92@gmail.com>
2024-11-08 12:44:31 -0800

    possible origin fix for cors

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'http://localhost:56418', 'https://qmovieflix.netlify.app', 'http://localhost:4200', 'https://ecanno8.github.io', 'https://ecanno8.github.io/MyFlix-AngularApp/', 'http://ec2-3-80-244-122.compute-1.amazonaws.com/', 'http://3.80.244.122/', 'http://cfcloudcomputingwebsite.s3-website-us-east-1.amazonaws.com/', 'http://cccfmovieapi.s3-website-us-east-1.amazonaws.com/', 'http://cccfmovieapi.s3-website-us-east-1.amazonaws.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'http://localhost:56418', 'https://qmovieflix.netlify.app', 'http://localhost:4200', 'https://ecanno8.github.io', 'https://ecanno8.github.io/MyFlix-AngularApp/', 'http://ec2-3-80-244-122.compute-1.amazonaws.com/', 'http://3.80.244.122', 'http://cfcloudcomputingwebsite.s3-website-us-east-1.amazonaws.com/', 'http://cccfmovieapi.s3-website-us-east-1.amazonaws.com/', 'http://cccfmovieapi.s3-website-us-east-1.amazonaws.com'];
 
 app.use(cors({
     origin: (origin, callback) => {

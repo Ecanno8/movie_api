db74ffccd511c9e5b1a01b67207246f243cd559b
Ecanno8 <ethancannon92@gmail.com>
2024-11-08 16:36:37 -0800

    cors

--- index.js
+++ index.js
@@ -30,16 +30,18 @@ app.use(morgan('dev'));
 
 let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'http://localhost:56418', 'https://qmovieflix.netlify.app', 'http://localhost:4200', 'https://ecanno8.github.io', 'https://ecanno8.github.io/MyFlix-AngularApp/', 'http://ec2-3-80-244-122.compute-1.amazonaws.com/', 'http://3.80.244.122/', 'http://cfcloudcomputingwebsite.s3-website-us-east-1.amazonaws.com/', 'http://cccfmovieapi.s3-website-us-east-1.amazonaws.com/', 'http://cccfmovieapi.s3-website-us-east-1.amazonaws.com'];
 
-app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-            let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-            return callback(new Error(message), false);
-        }
-        return callback(null, true);
-    }
-}));
+// app.use(cors({
+//     origin: (origin, callback) => {
+//         if (!origin) return callback(null, true);
+//         if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+//             let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//             return callback(new Error(message), false);
+//         }
+//         return callback(null, true);
+//     }
+// }));
+app.use(cors());
+
 
 let auth = require('./auth')(app);
 
